6a1fa679672a03fd16ad31eeb8cca1bcaf7fb858
uchoa <118028253+uchoa00@users.noreply.github.com>
2025-12-07 18:20:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-async function startScreenShare() {
-    const videoElement = document.getElementById("screenVideo");
+async function iniciarCompartilhamentoDeTela() {
+    const videoElement = document.getElementById("telaCompartilhada");
 
     try {
         const stream = await navigator.mediaDevices.getDisplayMedia({
             video: true,
             audio: false
         });
+
         videoElement.srcObject = stream;
+
     } catch (error) {
         alert("Erro ao compartilhar tela: " + error);
     }
